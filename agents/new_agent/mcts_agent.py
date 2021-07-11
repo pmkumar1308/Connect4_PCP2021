3c6b81715e72fa91aa91f303faa11fc06c2e08b9
pmkumar1308 <pmkumar1308@gmail.com>
2021-07-11 19:18:28 +0200

    After debugging and making it to run, still fills 6th column

--- agents/new_agent/mcts_agent.py
+++ agents/new_agent/mcts_agent.py
@@ -91,9 +91,10 @@ from agents.common import PlayerAction, SavedState, BoardPiece, PLAYER1, PLAYER2
 #         action = PlayerAction(int(col_))
 #         return action
 
-class MonteCarloTreeSearch():
+class MonteCarloTreeSearchNode():
 
     def __init__(self, state, player: BoardPiece, parent=None, parent_action=None):
+        self.initial_state = state.copy()
         self.state = state
         self.parent = parent
         self.parent_action = parent_action
@@ -122,20 +123,20 @@ class MonteCarloTreeSearch():
 
     def expand(self):
         action = self._untried_actions.pop()
-        next_state = self.move(self.state,action, self.player) #Should I be using self.state
-        child_node = MonteCarloTreeSearch(
+        next_state = self.move(self.state, action, self.player)  # Should I be using self.state
+        child_node = MonteCarloTreeSearchNode(
             next_state, player=self.player, parent=self, parent_action=action)
 
         self.children.append(child_node)
         return child_node
 
-    def is_terminal_node(self):
-        return self.is_game_over()
+    def is_terminal_node(self, curr_state,player):
+        return self.is_game_over(curr_state,player)
 
     def rollout(self):
-        current_rollout_state = self.state.copy()
+        current_rollout_state = self.state
         player_ = self.player
-        while not self.is_game_over():
+        while not self.is_game_over(current_rollout_state, player_):
             possible_moves = get_valid_columns(current_rollout_state)
 
             action = self.rollout_policy(possible_moves)
@@ -145,7 +146,7 @@ class MonteCarloTreeSearch():
             else:
                 player_ = PLAYER2
 
-        return self.game_result(player_)
+        return self.game_result(current_rollout_state, player_)
 
     def backpropagate(self, result):
         self._number_of_visits += 1.
@@ -156,7 +157,7 @@ class MonteCarloTreeSearch():
     def is_fully_expanded(self):
         return len(self._untried_actions) == 0
 
-    def best_child(self, c_param = 1.414):
+    def best_child(self, c_param=1.414):
 
         choices_weights = [(c.q() / c.n()) + c_param * np.sqrt((2 * np.log(self.n()) / c.n())) for c in self.children]
         return self.children[np.argmax(choices_weights)]
@@ -168,7 +169,7 @@ class MonteCarloTreeSearch():
     def _tree_policy(self):
 
         current_node = self
-        while not current_node.is_terminal_node():
+        while not current_node.is_terminal_node(self.state, self.player):
 
             if not current_node.is_fully_expanded():
                 return current_node.expand()
@@ -187,17 +188,17 @@ class MonteCarloTreeSearch():
         return self.best_child()
 
 
-    def is_game_over(self):
-        ans = connected_four(self.state, self.player)
+    def is_game_over(self, curr_state, player):
+        ans = connected_four(curr_state, player)
         return ans
 
     def move(self, board, action, player_playing):
         b = apply_player_action(board, action, player_playing, True)
         return b
 
-    def game_result(self,player_r):
+    def game_result(self, curr_state, player_r):
         curr_player = player_r
-        game_state = check_end_state(self.state, curr_player).name
+        game_state = check_end_state(curr_state, curr_player).name
         if game_state == 'IS_WIN' and self.player == PLAYER2:
             return 1
         if game_state == 'IS_DRAW':
@@ -205,14 +206,13 @@ class MonteCarloTreeSearch():
         if game_state == 'IS_WIN' and self.player == PLAYER1:
             return -1
 
+    def get_legal_actions(self):
+        return get_valid_columns(self.board)
 
 
 def generate_move_mcts(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState]
                        ) -> Tuple[PlayerAction, Optional[SavedState]]:
-    root = MonteCarloTreeSearch(state=board, player=player)
-    # print('a')
+    root = MonteCarloTreeSearchNode(state=board, player=player)
     selected_node = root.best_action()
-    # print('The selected node is',selected_node)
     action = PlayerAction(int(selected_node.parent_action))
     return action, saved_state
-
